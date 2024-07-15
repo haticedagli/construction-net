a63a6403f5957dc056cb892450c2d7423435a05a
Hatice Dağlı <haticedagli@Hatice-MBP.home>
2024-07-15 16:17:48 +0300

    inconstruction

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import constructionLogo from './assets/construction.png'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -9,24 +9,16 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={constructionLogo} className="logo" alt="Vite logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Sur İnşaat Fileleri</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Çok yakında sizinle burada buluşuyor olucaz.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        <p><a href="mailto:surinsaatfileleri@gmail.com">Mail göndermek için tıkla</a></p>
       </p>
     </>
   )
