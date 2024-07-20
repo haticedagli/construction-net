6af76a9fb9cc631026c58aa289ba36cf4e9be0a0
Hatice Dağlı <haticedagli@Hatice-MBP.home>
2024-07-20 23:30:46 +0300

    readme

---
+++ src/App 2.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import constructionLogo from './assets/construction.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <img src={constructionLogo} className="logo" alt="Vite logo" />
+      </div>
+      <h1>Sur İnşaat Fileleri</h1>
+      <div className="card">
+        <p>
+          Çok yakında sizinle burada buluşuyor olacağız
+        </p>
+      </div>
+      <p className="read-the-docs">
+        <p><a href="mailto:surinsaatfileleri@gmail.com">Mail göndermek için tıkla</a></p>
+      </p>
+    </>
+  )
+}
+
+export default App
